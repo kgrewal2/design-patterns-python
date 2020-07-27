65f99d5b7434cbddf720b1cedf6696d93e579247
Karandeep <kgrewal2@asu.edu>
2020-07-27 11:58:18 -0700

    :package: Prototype

---
+++ Creational/Prototype.py
@@ -0,0 +1,31 @@
+import copy
+class Car:
+    def __init__(self):
+        self.engine="3200cc"
+        self.color="Blue"
+        self.seats="5"
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.engine,self.color,self.seats)
+
+class Prototype:
+    def __init__(self):
+        self._toBeCloneObjects={}
+
+    def registerObject(self,name,obj):
+        self._toBeCloneObjects[name]=obj
+
+    def unregisterObject(self,name):
+        del self._toBeCloneObjects[name]
+
+    def clone(self, name, **args):
+        clonedObject=copy.deepcopy(self._toBeCloneObjects.get(name))
+        clonedObject.__dict__.update(args)
+        return clonedObject
+
+defaultCar = Car()
+prototype = Prototype()
+prototype.registerObject('basicCar', defaultCar)
+
+carOne=prototype.clone('basicCar', color="red")
+print(carOne)
