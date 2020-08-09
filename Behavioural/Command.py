edadd2807d4220332c871d9da49e2249d905aabb
Karandeep <kgrewal2@asu.edu>
2020-08-09 14:34:14 -0700

    :package: Adding Command

---
+++ Behavioural/Command.py
@@ -0,0 +1,37 @@
+class Command:
+    def execute(self):
+        pass
+
+
+class Copy(Command):
+    def execute(self):
+        print("Copying")
+
+
+class Paste(Command):
+    def execute(self):
+        print("Pasting")
+
+
+class Save(Command):
+    def execute(self):
+        print("Saving")
+
+
+class Macro:
+    def __init__(self):
+        self.commands = []
+
+    def add(self, command):
+        self.commands.append(command)
+
+    def sum(self):
+        for c in self.commands:
+            c.execute()
+
+
+macro = Macro()
+macro.add(Copy())
+macro.add(Paste())
+macro.add(Save())
+macro.sum()
