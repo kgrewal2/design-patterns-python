de35b919f5ce25df64391c459ea712198e136846
Karandeep <kgrewal2@asu.edu>
2020-08-09 20:50:21 -0700

    :package: Adding State

---
+++ Behavioural/State.py
@@ -0,0 +1,37 @@
+class ATMState():
+    name = "state"
+    allowed = []
+
+    def go_next(self, state):
+        if state.name in self.allowed:
+            print("Current State changes from", self, "to", state.name)
+            self.__class__ = state
+        else:
+            print("Give State change not possible")
+
+    def __str__(self):
+        return self.name
+
+
+class OffState(ATMState):
+    name = "off"
+    allowed = ["on"]
+
+
+class OnState(ATMState):
+    name = "on"
+    allowed = ["off"]
+
+
+class ATM():
+    def __init__(self):
+        self.current = OffState()
+
+    def set_state(self, state):
+        self.current.go_next(state)
+
+
+atm = ATM()
+atm.set_state(OnState)
+atm.set_state(OffState)
+atm.set_state(OnState)
